0522da2d9e3329a4e5bbc0878a546bf756c0c03c
ruchi0909 <88983102+ruchi0909@users.noreply.github.com>
2022-03-18 21:52:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var word = "Skill Safari"
+console.log(word.length)
+console.log(word.toUpperCase())
+console.log(word.indexOf("a"))
+console.log("Steve Rogers: Big man in a suit of armor. Take that off, what are you? \n Tony Stark: Genius, billionaire, playboy, philanthropist.")
+
+function BMI(height,weight)
+{
+    return weight/(height*height)
+}
+
+console.log(BMI(2.75,100))
